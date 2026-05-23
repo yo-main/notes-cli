50d15ce1bca745c4c7548ea9706207d14e1a7564
Romain Arnal <arnal.romain@gmail.com>
2026-05-23 13:25:22 +0200

    Implement show command

--- notes.sh
+++ notes.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
-DATABASE=./database
+TODO=./data/todo
+DONE=./data/done
 
 function new_note() {
 
@@ -19,20 +20,48 @@ priority: medium
   if [[ "$title" == "#" ]]; then
     return
   fi
- 
-  
+
+  # if the title is like #aze, let's make it # aze
+  if [[ ${title:1:1} != " " ]]; then
+    title="# ${title:1}"
+  fi
+   
   id="id: $(uuidgen | cut -c1-8)"
   content="$id
 $(cat $note_tmp_file)
 ---
-$(cat $DATABASE)  "
+$(cat $TODO)"
 
-  echo "$content" > "$DATABASE"
+  echo "$content" > "$TODO"
 
 }
 
+function get_todo_notes() {
+  readarray -d '' notes < <(
+    awk -v RS='---' '
+    {
+      gsub(/^[ \n]+|[ \n]+$/, "", $0)  # trim
+
+      if ($0 ~ /[^[:space:]]/) {       # only output non-empty blocks
+        printf "%s\0", $0
+      }
+    }
+  ' "$TODO"
+  )
+}
+
 function show_notes() {
-  
+  get_todo_notes
+
+  note_tmp_file=$(mktemp)
+
+  for note in "${notes[@]}"; do
+    title="${note#*# }"
+    echo "[] ${title}" >> "$note_tmp_file"
+  done
+
+  alacritty -T "new-note" -e hx "$note_tmp_file"
+
 }
 
 case "$1" in
