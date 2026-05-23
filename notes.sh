e1376c01893847ae0ea78c5b3c90c03c1b871b4d
Romain Arnal <arnal.romain@gmail.com>
2026-05-23 22:09:18 +0200

    refacto to work with multiple files

--- notes.sh
+++ notes.sh
@@ -5,95 +5,53 @@ DONE=./data/done
 
 function new_note() {
 
-  note_tmp_file=$(mktemp)
+  id="$(uuidgen | cut -c1-8)"
+  filename="$TODO/$id"
 
   template="date: $(date --iso)
 priority: medium
 
 #"
 
-  echo "$template" > "$note_tmp_file"
+  echo "$template" > "$filename"
 
-  open_file "$note_tmp_file" +4
+  open_file "$filename" +4
 
-  title=$(sed -n "4p" "$note_tmp_file" | tr -d '[:space:]')
+  title=$(sed -n "4p" "$filename" | tr -d '[:space:]')
   if [[ "$title" == "#" ]]; then
+    rm "$filename"
     return
   fi
-
-  # if the title is like #aze, let's make it # aze
-  if [[ ${title:1:1} != " " ]]; then
-    title="# ${title:1}"
-  fi
-   
-  id="id: $(uuidgen | cut -c1-8)"
-  content="$id
-$(cat $note_tmp_file)
----
-$(cat $TODO)"
-
-  echo "$content" > "$TODO"
-
 }
 
-function get_todo_notes() {
-  readarray -d '' notes < <(
-    awk -v RS='---' '
-    {
-      gsub(/^[ \n]+|[ \n]+$/, "", $0)  # trim
-
-      if ($0 ~ /[^[:space:]]/) {       # only output non-empty blocks
-        printf "%s\0", $0
-      }
-    }
-  ' "$TODO"
-  )
-}
 
 function mark_note_as_done() {
   todo_file="$1"
 
   done_ids=$(cat "$todo_file" | grep "[x]" | awk '{print $2}')
 
-  get_todo_notes
+  todos=$(ls "$TODO")
 
   todo=""
-  done=$(cat "$DONE")
 
-  for note in "${notes[@]}"; do
-    note_id=$(echo "$note" | grep "id: ")
-    note_id=${note_id:4}
-    marked_as_done=0
-
+  for filename in "${todos[@]}"; do
     for done_id in "${done_ids[@]}"; do
-      if [[ "$done_id" == "$note_id" ]]; then
-        done="${note}\n---\n${done}"
-        marked_as_done=1
+      if [[ "$done_id" == "$filename" ]]; then
+        mv "$TODO/$filename" "$DONE/$filename"
         break
       fi
     done
-
-    if [[ "$marked_as_done" == 0 ]]; then
-        todo="${note}\n---\n${todo}"
-    fi
-    
   done
-
-  echo -e "$todo" > "$TODO"
-  echo -e "$done" > "$DONE"
 }
 
 function view_note() {
   view_id="$1"
 
-  get_todo_notes
+  todos=$(ls "$TODO")
 
-  note_tmp_file=$(mktemp)
-
-  for note in "${notes[@]}"; do
-    note_id=$(echo "$note" | grep "id: ")
-    if [[ "${note_id:4}" == "$view_id" ]]; then
-      echo -e "$note"
+  for filename in "${todos[@]}"; do
+    if [[ "${filename}" == "$view_id" ]]; then
+      echo -e $(cat "$TODO/$filename")
       break
     fi
   done
@@ -101,43 +59,22 @@ function view_note() {
 
 function open_note() {
   view_id="$1"
-
-  get_todo_notes
-
-  note_tmp_file=$(mktemp)
-
-  for note in "${notes[@]}"; do
-    note_id=$(echo "$note" | grep "id: ")
-    if [[ "${note_id:4}" == "$view_id" ]]; then
-      echo -e "${note}" > "$note_tmp_file"
-      break
-    fi
-  done
-
-  open_file "$note_tmp_file" +5
-
-  todo=""
-  for note in "${notes[@]}"; do
-    note_id=$(echo "$note" | grep "id: ")
-    if [[ "${note_id:4}" == "$view_id" ]]; then
-      todo="$todo"\n---\n$(cat "$note_tmp_file")"
-    else
-      todo="$(echo "$note")\n---\n${todo}"
-    fi
-  done
-
-  echo "$todo" > "$TODO"
+  open_file "$TODO/$view_id" +4
 }
 
 function list_notes() {
-  get_todo_notes
+  todos=$(ls "$TODO")
 
   note_tmp_file=$(mktemp)
 
-  for note in "${notes[@]}"; do
-    title=$(echo "$note" | grep "# ")
-    note_id=$(echo "$note" | grep "id: ")
-    printf "[] %s - %s\n"  "${note_id:4}" "${title:2}" >> "$note_tmp_file"
+  for filename in "${todos[@]}"; do
+    if [[ -z "$filename" ]]; then
+      echo "No notes"
+      return
+    fi
+
+    title=$(cat "$TODO/$filename" | grep "# ")
+    printf "[] %s - %s\n"  "${filename}" "${title:2}" >> "$note_tmp_file"
   done
 
   open_file "$note_tmp_file"
